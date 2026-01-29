092dddc462181598e5d8b90ba961fcaf49528d89
Adnan <aadnanmtcontact@gmail.com>
2026-01-29 21:00:59 +0700

    feat: first test lixanime new style

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="p-10 text-center">
+      <h1 className="text-4xl font-bold text-purple-500">Lixanime</h1>
+      <p className="mt-4 text-slate-400">Project baru dimulai!</p>
+    </div>
+  )
+}
+
+export default App
