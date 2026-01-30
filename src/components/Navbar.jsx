5f003082dff2ed53e557d09bad8d5253df32c0ff
Adnan <aadnanmtcontact@gmail.com>
2026-01-30 20:37:47 +0700

    Feat: animation on Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { Search, Tv } from 'lucide-react';
+import { Search, Star } from 'lucide-react';
 
 const Navbar = () => {
   return (
@@ -7,8 +7,8 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 h-16 flex justify-between items-center">
         {/* Logo */}
         <Link to="/" className="flex items-center gap-2 group">
-            <div className="bg-purple-600 p-1.5 rounded-lg group-hover:scale-110 transition-transform">
-                <Tv size={20} className="text-white" />
+            <div className="bg-purple-600 p-1.5 rounded-lg group-hover:-translate-x-1 group-hover:rotate-360 transition-transform">
+                <Star size={24} className="text-white" />
             </div>
             <span className="text-xl font-bold bg-gradient-to-r from-purple-400 to-pink-400 bg-clip-text text-transparent">
                 Lixanime
@@ -22,7 +22,7 @@ const Navbar = () => {
                 placeholder="Cari anime..."
                 className="bg-slate-900/50 border border-white/10 rounded-full py-2 pl-10 pr-4 text-sm focus:outline-none focus:ring-2 focus:ring-purple-500/50 w-64 transition-all"
             />
-            <Search className="absolute left-3 top-2.5 text-slate-500" size={16} />
+            <Search className="absolute left-3 top-2.5 text-slate-500" size={19} />
         </div>
       </div>
     </nav>
